6b71ef5c394f6c1e6d4eee341737e3b64b31bc9c
Arindam Majumder <Arindam Majumder>
2016-12-11 22:30:16 +0530

    Moved data to controllers

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -32,10 +32,46 @@ module.exports.homelist = function(req, res){
 
 /* GET 'location info' page */
 module.exports.locationInfo = function(req, res){
-	res.render('location-info', {title: 'Location info' });
+	res.render('location-info', {
+		title: 'KFC',
+		pageHeader: {title: 'KFC'},
+		sidebar: {
+			context: 'is on Loc8r because it\'s famous for its Chicken Wings, Bucket and other finger licking crisp and smoked chicken items.',
+			callToAction: 'If you\'ve been here and you like it - or if you don\'t - please leave a review comment to help people just like you.'
+		},
+		location: {
+			name: 'KFC',
+			address: 'Spot 18 Mall, Shivar Chowk, Rahatani, Pimple Saudagar, Pune',
+			rating: 3,
+			coords: {lat: 18.593465, lng: 73.786001},
+			openingTimes: {
+				days: 'Monday - Sunday',
+				opening: '11:00am',
+				closing: '11:00pm',
+				closed: false
+			},
+			contactInfo: '020 30162053',
+			averageCost: 'Rs550 for two (approx.)',
+			reviews: [{
+				author: 'Arindam Majumder',
+				rating: 4,
+				timestamp: '11 Dec 2016',
+				reviewText: 'Ambience is good as it\'s in Spot 18 Mall. We were here at around 7:30 pm and had KFC Chicken Wings and Smokey Chicken which was awesome in taste.'
+			},{
+				author: 'Yogesh Tandale',
+				rating: 3,
+				timestamp: '20 Nov 2016',
+				reviewText: 'Had takeaway for Hot & Crispy chicken. Overall taste was just OK. It wasn\'t that warm/hot (consider my travel time to be 5-10 min). It wasn\'t even spicy a little bit, so after crisp cover what remains is only boiled chicken.'
+			}
+			]
+		}
+	});
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
-	res.render('location-review-form', {title: 'Add review' })
+	res.render('location-review-form', {
+		title: 'Review KFC on Loc8r',
+		pageHeader: {title: 'Review KFC'}
+	})
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function(req, res){
-	res.render('generic-text', { title: 'About Loc8r' });
+	res.render('generic-text', {
+		title: 'About Loc8r',
+		context: 'Loc8r was created to help people find restaurants nearby along with all necessary details.\n\n We show the updated data about any restaurant listed in this site and we ensure that the information is accurate and complete.\n\n\n\n'
+	});
 };
